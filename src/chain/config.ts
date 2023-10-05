26f54b54908874bf125a0a480e2bc454d10ac292
vladilen11 <vladilenaksana@163.com>
2023-10-05 17:57:30 +0800

    update

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -1,12 +1,8 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
-const NETWORK: NetworkType = 'localnet';
+const NETWORK: NetworkType = 'devnet';
 
-const PACKAGE_ID = '0x3dda12e0d4a753dd82516a8a4f49ec50340beb2b6702424a8e1b54ea69dc1bff'
-const WORLD_ID = '0xcf20db83cec787be9eb15aec58230d6f0c363d2fa300babd0f56fc209f9b4bec'
+const PACKAGE_ID = '0x8e14c42fb382bbc9d20c4b0181475896473ed3e2a2affc640b5e3a2c99fbe504';
+const WORLD_ID = '0x7c8e3197abf34b2135ac53d63af0a98d7b4818990119ba3a40ad5018f5f900c1';
 
-export {
-    NETWORK,
-    PACKAGE_ID,
-    WORLD_ID,
-}
+export { NETWORK, PACKAGE_ID, WORLD_ID };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,14 @@
-import {getMetadata, Obelisk,TransactionBlock} from "@0xobelisk/client";
-import {useEffect} from "react";
+import {DevInspectResults, getMetadata, Obelisk, TransactionBlock} from "@0xobelisk/client";
+import {useEffect, useState} from "react";
 import {useAtom} from "jotai";
 import { Map, Dialog } from "../../components";
 import {Value} from "../../jotai";
 import { useRouter } from "next/router";
+import { useDispatch, useSelector } from 'react-redux';
 import {NETWORK, PACKAGE_ID, WORLD_ID} from "../../chain/config";
 import {obeliskConfig} from "../../../obelisk.config";
 import PRIVATEKEY from "../../chain/key";
-
+import { setHero, setMapData } from "../../store/actions"
 
 type data = {
     type:string;
@@ -17,10 +18,12 @@ type data = {
 }
 
 const Home = () =>{
+  let dispatch = useDispatch();
     const router = useRouter()
     const [value,setValue] = useAtom(Value)
+    const [isLoading, setIsLoading] = useState(false)
 
-    const counter = async () => {
+    const rpgworld = async () => {
         const metadata = await getMetadata(NETWORK, PACKAGE_ID);
         const obelisk = new Obelisk({
             networkType: NETWORK,
@@ -28,31 +31,160 @@ const Home = () =>{
             metadata: metadata,
             secretKey:PRIVATEKEY
         });
-        const tx = new TransactionBlock()
-        const world = tx.pure(WORLD_ID)
-        const params = [
-            world,
-        ]
-        const new_tx = await obelisk.tx.counter_system.inc(tx, params,true) as TransactionBlock;
-        const response = await obelisk.signAndSendTxn(
-            new_tx
 
-        )
-        if (response.effects.status.status == 'success') {
-            const metadata = await getMetadata(NETWORK, PACKAGE_ID);
-            const obelisk = new Obelisk({
-                networkType: NETWORK,
-                packageId: PACKAGE_ID,
-                metadata: metadata,
-            });
-            const component_name = Object.keys(obeliskConfig.singletonComponents)[0]
-            const component_value = await obelisk.getComponentByName(WORLD_ID,component_name)
-            const content = component_value.data.content as data
-            const value = content.fields.value.fields.data
-            setValue(value)
-        }
+
+        const player_id = "0xa9288f6ebe5ea276cd64230458d0c6c80725ca4c0dfc42a6d4af08a96832c52c"
+        const player_data = await obelisk.getEntity(WORLD_ID, 'position', player_id)
+        console.log(player_data)
+        console.log(JSON.stringify(player_data))
+        const stepLength = 2.5;
+        console.log( player_data[0] * stepLength)
+        dispatch(setHero({
+          name: player_id,
+          position: { left: player_data[0] * stepLength, top: player_data[1] * stepLength },
+        }))
+    
+
+        const map_data = await obelisk.getEntity(WORLD_ID, 'map')
+
+        dispatch(setMapData({map: map_data[0], type: "green",
+        ele_description: {
+          walkable: [0],
+          tussock: [20],
+          flower: [22],
+          
+          object: [40, 41],
+          sprite: [42, 43],
+          old_man: [43],
+          fat_man: [44],
+    
+          ground_top_1: [50],
+          ground_top_2: [51],
+          ground_top_3: [52],
+          ground_middle_1: [53],
+          ground_middle_2: [54],
+          ground_middle_3: [55],
+          ground_bottom_1: [56],
+          ground_bottom_2: [57],
+          ground_bottom_3: [58],
+    
+          water_top_1: [60],
+          water_top_2: [61],
+          water_top_3: [62],
+          water_middle_1: [63],
+          water_middle_2: [64],
+          water_middle_3: [65],
+          water_bottom_1: [66],
+          water_bottom_2: [67],
+          water_bottom_3: [68],
+          
+          obelisk_top: [70],
+          obelisk_middle_1: [71],
+          obelisk_middle_2: [72],
+          obelisk_bottom: [73],
+          obelisk_bottom_left: [74],
+          obelisk_bottom_right: [75],
+    
+          tree_top: [80],
+          tree_bottom: [81],
+          unwalkable: [83],
+          rocks: [84],
+    
+          big_house_1: [100],
+          big_house_2: [101],
+          big_house_3: [102],
+          big_house_4: [103],
+          big_house_5: [104],
+          big_house_6: [105],
+          big_house_7: [106],
+          big_house_8: [107],
+          big_house_9: [108],
+          big_house_10: [109],
+          big_house_11: [110],
+          big_house_12: [111],
+          big_house_13: [112],
+          big_house_14: [113],
+          big_house_15: [114],
+          big_house_16: [115],
+          big_house_17: [116],
+          big_house_18: [117],
+          big_house_19: [118],
+          big_house_20: [119],
+          big_house_21: [120],
+          big_house_22: [121],
+          big_house_23: [122],
+          big_house_24: [123],
+          big_house_25: [124],
+          big_house_26: [125],
+          big_house_27: [126],
+          big_house_28: [127],
+          big_house_29: [128],
+          big_house_30: [129],
+          big_house_31: [130],
+          big_house_32: [131],
+          big_house_33: [132],
+          big_house_34: [133],
+          big_house_35: [134],
+    
+    
+          small_house_1: [135],
+          small_house_2: [136],
+          small_house_3: [137],
+          small_house_4: [138],
+          small_house_5: [139],
+          small_house_6: [140],
+          small_house_7: [141],
+          small_house_8: [142],
+          small_house_9: [143],
+          small_house_10: [144],
+          small_house_11: [145],
+          small_house_12: [146],
+          small_house_13: [147],
+          small_house_14: [148],
+          small_house_15: [149],
+          small_house_16: [150],
+          small_house_17: [151],
+          small_house_18: [152],
+          small_house_19: [153],
+          small_house_20: [154],
+          small_house_21: [155],
+          small_house_22: [156],
+          small_house_23: [157],
+          small_house_24: [158],
+          small_house_25: [159],
+    
+          house_land: [160],
+          house_stree: [161],
+        },
+        events: [],
+        map_type: "event",}))
+
+        // const new_tx = await obelisk.tx.map_comp.get(tx, params,true) as TransactionBlock;
+        // const response = await obelisk.signAndSendTxn(
+        //     new_tx
+        // )
+
+        // if (response.effects.status.status == 'success') {
+        //     const metadata = await getMetadata(NETWORK, PACKAGE_ID);
+        //     const obelisk = new Obelisk({
+        //         networkType: NETWORK,
+        //         packageId: PACKAGE_ID,
+        //         metadata: metadata,
+        //     });
+        //     const component_name = Object.keys(obeliskConfig.singletonComponents)[0]
+        //     const component_value = await obelisk.getComponentByName(WORLD_ID,component_name)
+        //     const content = component_value.data.content as data
+        //     const value = content.fields.value.fields.data
+        //     setValue(value)
+        // }
+        setIsLoading(true)
     }
 
+    useEffect(() => {
+        if (router.isReady){
+          rpgworld()
+        }
+    }, [router.isReady]);
 
 
     // useEffect(() => {
@@ -77,7 +209,9 @@ const Home = () =>{
     //         query_counter()
     //     }
     // }, [router.isReady]);
-    return (
+    if (isLoading) {
+
+      return (
         <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
         <div style={{minHeight: '1px', display: 'flex', marginBottom: '20px', position: 'relative'}}>
           <Map />
@@ -109,6 +243,9 @@ const Home = () =>{
         //     </div>
         // </div>
     )
+    } else {
+      <></>
+    }
 }
 
 export default Home
