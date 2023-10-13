1899fdb001a905435e93a8018fb2b551f6ce574c
vladilen11 <vladilenaksana@163.com>
2023-10-13 15:29:02 +0800

    update package.json script

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -2,8 +2,8 @@ type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 const NETWORK: NetworkType = 'localnet';
 
-const PACKAGE_ID = '0x5e130c003cfc37f78aaf496848bb90dac09b079a6dc1f9e2d6587c8ffd2595f2'
-const WORLD_ID = '0xe0d9c054adff7d347ba0f7b835d06c88b854171f9232ca8fda4c7bab384f9ea9'
+const PACKAGE_ID = '0x4bf31a2f011857a460b1e713622cb039c53cbc8008a798c66d81b6884fee5848'
+const WORLD_ID = '0x9e1f801de78cabb703b1ba97b47402cebc09278ed69ad33272aca9cd8c824360'
 
 export {
     NETWORK,
